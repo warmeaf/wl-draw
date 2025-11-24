b915f1e29e7fefdc6d87db09447f6b73a52f6d76
nextc <1391040917@qq.com>
2025-11-24 11:57:34 +0800

    feat: add GitHub Actions workflow for deploying VitePress documentation to GitHub Pages and set base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
